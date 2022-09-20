8c0505e93840241884c9267974acb5403e6092eb
StanislavLabanov <102102899+StanislavLabanov@users.noreply.github.com>
2022-09-20 21:03:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <div dangerouslySetInnerHTML={{
+        __html: `
+        <video
+          loop
+          muted
+          autoplay
+          playsinline
+          src="./earth 0.5x.mp4"
+        />,
+      ` }}></div>
+      <header>
+        <img src="./SHELTERZ.svg" className="right_block" />
+        <div className="left_block">
+          <div className="btn_one">Зарегестрировать свой объект</div>
+          <div className="btn_two">Зарегестрироваться</div>
+          <img className="user" src="./Group 88.svg" />
+          <img className="burger" src="./Group 87.svg" />
+        </div>
       </header>
+      <h1>Сервис бронирования жилья<br />с полным возвратом средств</h1>
+      <div className="data">
+        <img src="./Group 368.svg" />
+      </div>
+      <div className="hotel">Бронируйте отели, квартиры,<br />хостелы и другие объекты</div>
+      <div className="cashback">с кешбеком 100%</div>
     </div>
   );
 }
